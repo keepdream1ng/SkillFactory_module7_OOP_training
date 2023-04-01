608b9a66daa0bf8b012827dff75d42ec6f681a8e
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-01 19:46:08 +0530

    Add project files.

---
+++ SkillFactory.Delivery_system/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.Delivery_system
+{
+    class Order<TDelivery> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+    }
+}
