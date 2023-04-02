c5bf92e638b5cf41742786cce4965e441b33215e
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-02 16:03:09 +0530

    filling the Order class and connected things

--- SkillFactory.Delivery_system/Order.cs
+++ SkillFactory.Delivery_system/Order.cs
@@ -8,16 +8,27 @@ namespace SkillFactory.Delivery_system
 {
     class Order<TDelivery> where TDelivery : Delivery
     {
+        private static int _count = 0;
+        public int Number { get; private set; }
+        public string Description { get => this.ToString(); }
+        public Status Status;
+
+        public Client Owner;
         public TDelivery Delivery;
+        public Product[] OrderPackage;
 
-        public int Number;
+        public Order(Client owner, Product[] orderPackage)
+        {
+            Number = ++_count;
+            Status = Status.Pending;
+            Owner = owner;
+            OrderPackage = orderPackage;
+        }
 
-        public string Description;
 
         public void DisplayAddress()
         {
             Console.WriteLine(Delivery.Address);
         }
-
     }
 }
---
+++ SkillFactory.Delivery_system/enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.Delivery_system
+{
+    public enum DeliveryType
+    {
+        HomeDelivery = 0,
+        PickUpDelivery,
+        ShopDelivery
+    }
+
+    public enum Status
+    {
+        Cancelled = 0,
+        Pending,
+        Collecting,
+        Shipment,
+        Awaiting,
+        Delivered
+    }
+}
