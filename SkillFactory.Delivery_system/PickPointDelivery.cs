f2f0d572efe75d46ad3eb0822d550dc2660e5c24
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-06 15:04:29 +0530

    added logic to PickPointDelivery

--- SkillFactory.Delivery_system/PickPointDelivery.cs
+++ SkillFactory.Delivery_system/PickPointDelivery.cs
@@ -8,8 +8,32 @@ namespace SkillFactory.Delivery_system
 {
     public class PickPointDelivery : Delivery
     {
+        private ShippingUnit _pickedShippingUnit;
+        public ShippingUnit PickedShippingUnit
+        {
+            get
+            {
+                if (_pickedShippingUnit == null)
+                {
+                    return _pickedShippingUnit = PickAvailable<ShippingUnit>();
+                }
+                return _pickedShippingUnit;
+            }
+        }
+
         public PickPointDelivery(string adress) : base(adress)
         {
+        }
+
+        public override void StartDelivery()
+        {
+            PickedShippingUnit.StartShipment(Address);
+            PickedShippingUnit.DeliverPackage(Address);
+        }
+
+        public override string ToString()
+        {
+            return $"Delivery to {Address}, with contractor {PickedShippingUnit.Id}";
         }
     }
 }
